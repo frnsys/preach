e282f556e0dfc347d1fe45369e35498c38a477a7
Francis Tseng <f+accounts@frnsys.com>
2016-05-24 09:01:11 -0400

    fixed watch command

--- preach/note.py
+++ preach/note.py
@@ -11,8 +11,13 @@ class Note():
         self.path = path
         _, self.filename = os.path.split(self.path)
         self.title, self.ext = os.path.splitext(self.filename)
-        self.content = open(path, 'r').read()
         self.dir = os.path.dirname(self.path)
+
+    @property
+    def content(self):
+        """return content dynamically,
+        in case the file changes"""
+        return open(self.path, 'r').read()
 
     @property
     def images(self):
