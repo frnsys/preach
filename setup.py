4c8c0b068749bfc87b85f888af18bb90ac012d79
Francis Tseng <f+accounts@frnsys.com>
2016-05-23 23:00:43 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='preach',
-    version='0.0.1',
+    version='0.0.2',
     description='simple presentation builder',
     url='https://github.com/frnsys/preach',
     author='Francis Tseng',
