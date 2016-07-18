a4f132dd68e7d32ba163f5d3e2b60525e77d2a09
Francis Tseng <f+accounts@frnsys.com>
2016-07-18 12:19:15 -0400

    better slide scaling

--- preach/__init__.py
+++ preach/__init__.py
@@ -15,7 +15,7 @@ def cli():
 @click.argument('note')
 @click.option('-o', '--outdir', help='output directory', default='_build')
 @click.option('-w', '--watch', is_flag=True, help='watch for changes')
-@click.option('-s', '--style', help='stylesheet to use', default='default')
+@click.option('-s', '--style', help='stylesheet to use', default=None)
 def compile(note, outdir, watch, style):
     n = Note(os.path.join(os.getcwd(), note))
     f = partial(compile_note, outdir=outdir, stylesheet=style)
